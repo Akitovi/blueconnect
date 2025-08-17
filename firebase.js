4d9d91e3ec3618d7d0962ed8eb9b822183d2c296
akitovi <shoheakitovi@gmail.com>
2025-08-17 17:53:49 +0530

    move firebase key to.env and update gitignore

--- firebase.js
+++ firebase.js
@@ -8,12 +8,12 @@ import {
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "DUMMY_KEY",
-  authDomain: "blueconnect-47909.firebaseapp.com",
-  projectId: "blueconnect-47909",
-  storageBucket: "blueconnect-47909.appspot.com",
-  messagingSenderId: "585057120917",
-  appId: "1:585057120917:web:3906c4e4f2ed106b78e19c"
+   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
